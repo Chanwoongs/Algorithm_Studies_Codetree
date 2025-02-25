81b0976535f1d0f5f6527c1fc22386d2b66f086f
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-02-25 23:19:10 +0900

    Title: 블럭쌓는 명령2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250225/블럭쌓는 명령2/block-stacking-commands2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, K, m = 0;
+int A[100], B[100];
+vector<int> v;
+
+int main() {
+    cin >> N >> K;
+    v.resize(N, 0);
+
+    for (int i = 0; i < K; i++) {
+        cin >> A[i] >> B[i];
+    }
+
+    // Write your code here!
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = A[i]; j < B[i] + 1; j++)
+        {
+            v[j]++;
+
+            if (m < v[j])
+            {
+                m = v[j];
+            }
+        }
+    }
+
+    cout << m;
+
+    return 0;
+}

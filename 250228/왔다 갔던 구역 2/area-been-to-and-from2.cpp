700887fef5430812d7b7bffccacfc82f07908056
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-02-28 10:25:10 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250228/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int offset = 1000;
+
+int num, res = 0;
+char c;
+int n;
+vector<int> v;
+vector<pair<int, char>> com;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    v.resize(3000);
+    com.resize(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num >> c;
+        com[i].first = num;
+        com[i].second = c;
+    }
+
+    int pos = 1500;
+    for (int i = 0; i < com.size(); i++)
+    {
+        int move = com[i].first;
+
+        if (com[i].second == 'R')
+        {
+            for (int j = pos; j < pos + move; j++)
+            {
+                v[j]++;
+            }
+            pos += move;
+        }
+        else
+        {
+            for (int j = pos; j > pos - move; j--)
+            {
+                v[j]++;
+            }
+            pos -= move;
+        }
+    }
+
+    for (auto& ele : v)
+    {
+        if (ele >= 2)
+        {
+            res++;
+        }
+    }
+
+    cout << res << '\n';
+}

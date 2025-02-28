3fa9bc5cdafb1037a87b05bbf76f9206b069c7e8
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-02-28 09:46:58 +0900

    Title: 최대로 겹치는 구간, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250228/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int n, m = 0;;
+int x1[100], x2[100], arr[100];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+    }
+
+    memset(arr, 100, sizeof(int));
+
+    // Please write your code here.
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = x1[i]; j <= x2[i]; j++)
+        {
+            ++arr[j];
+            if (m < arr[j])
+            {
+                m = arr[j];
+            }
+        }
+    }
+
+    cout << m << '\n';
+
+    return 0;
+}
